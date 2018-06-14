b2801be9894d3860bcc7efac8df6ed8127d2ff5a
chenzhongxiang <chenzhongxiang@youbangsoft.com>
2018-06-14 09:29:40 +0800

    #合入经营数据地区

--- src/app/views/data-statistics/operating-data/operating-data.component.ts
+++ src/app/views/data-statistics/operating-data/operating-data.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup} from '@angular/forms';
 import { OperatingDataServiceNs } from './operating-data.service';
+import { MerchantDataServiceNs } from '../merchant-data/merchant-data.service';
 import { Router, ActivatedRoute } from '@angular/router';
 enum MerchantStatusE {
   all = 0,
@@ -18,12 +19,15 @@ export class OperatingDataComponent implements OnInit {
   searchParam: OperatingDataServiceNs.OperatingDataSearchReqModel;
   merchantStatuMapping: {[k: string]: string} = {};
   constructor(private fb: FormBuilder, private router: Router,
-              private operatingDataService: OperatingDataServiceNs.OperatingDataService) {
+              private operatingDataService: OperatingDataServiceNs.OperatingDataService,
+              private merchantDataService: MerchantDataServiceNs.MerchantDataService) {
     this.searchParam = {};
 
     this.merchantStatuMapping[`=${MerchantStatusE.all}`] = '全部';
     this.merchantStatuMapping[`=${MerchantStatusE.inUse}`] = '在用';
     this.merchantStatuMapping[`=${MerchantStatusE.blockUp}`] = '停用';
+
+    this.loadAreaDataHandler = this.loadAreaData.bind(this);
   }
 
   tableDataSet = [];
@@ -31,6 +35,36 @@ export class OperatingDataComponent implements OnInit {
   pageSize = 10;
   total = 1;
   loading = false;
+  loadAreaDataHandler = null;
+
+  public loadAreaData(node: any, index: number):  PromiseLike<any> {
+    const idStr = node.value || '0';
+    const self = this;
+    return new Promise((resolve) => {
+      self.merchantDataService.getAreaList(idStr).then((data) => {
+        node.children = this.packAreaData(data.value, index === 1);
+        resolve();
+      });
+    });
+  }
+
+  private packAreaData(dataList: MerchantDataServiceNs.AreaDataModel[], isLast?: boolean): MerchantDataServiceNs.AreaDataModel[] {
+    dataList.forEach(item => {
+      item.label = item.areaName;
+      item.value = item.id;
+      item.isLeaf = isLast;
+    });
+    return dataList;
+  }
+
+
+  public areaChanges(value: any): void {
+    console.info(value);
+    if (!value) {
+      return;
+    }
+    this.validateForm.get('companyAreaId').setValue(value);
+  }
   searchData(reset: boolean = false): void {
     if (reset) {
       this.pageIndex = 1;
@@ -40,7 +74,7 @@ export class OperatingDataComponent implements OnInit {
       pageSize: this.pageSize,
       filters : {
         companySearch: this.searchParam.companySearch,
-        companyAreaId: this.searchParam.companyAreaId,
+        companyAreaId: this.searchParam.companyAreaId ? this.searchParam.companyAreaId[this.searchParam.companyAreaId.length - 1 ] : '',
         companyState: this.searchParam.companyState,
       }
     };
@@ -84,7 +118,7 @@ export class OperatingDataComponent implements OnInit {
 
     this.validateForm = this.fb.group({
       companySearch: [''],
-      companyAreaId: [''],
+      companyAreaId: [],
       companyState: ['0']
     });
     this.searchData();
